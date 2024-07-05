08bbb6fdc652eee52d96d702f0b87da696d8406c
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-06 03:37:58 +0530

    price

---
+++ frontend/src/components/PriceFilter.tsx
@@ -0,0 +1,27 @@
+type Props = {
+  selectedPrice?: number;
+  onChange: (value?: number) => void;
+};
+
+const PriceFilter = ({ selectedPrice, onChange }: Props) => {
+  return (
+    <div>
+      <h4 className='mb-2 font-semibold text-md'> Max Price</h4>
+      <select
+        className='w-full p-2 border rounded-md'
+        value={selectedPrice}
+        onChange={(event) =>
+          onChange(
+            event.target.value ? parseInt(event.target.value) : undefined
+          )
+        }>
+        <option value=''>Select Max Price</option>
+        {[50, 100, 200, 300, 500].map((price) => (
+          <option value={price}>{price}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default PriceFilter;
