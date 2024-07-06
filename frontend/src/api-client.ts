dd3d6457df0da967b514af6efb4ed96e690489cd
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-06 15:53:52 +0530

    get hotel route

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -162,3 +162,11 @@ export const searchHotels = async (
 
   return response.json();
 };
+
+export const fetchHotels = async (): Promise<HotelType[]> => {
+  const response = await fetch(`${API_BASE_URL}/api/hotels`);
+  if (!response.ok) {
+    throw new Error("Error fetching hotels");
+  }
+  return response.json();
+};
